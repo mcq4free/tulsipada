22e3c06ab8d745e994eb8de94790c5ec38e01418
Tulsipada Das <tulsipada@athenaehosting.com>
2023-09-02 07:37:34 +0530

    setup

---
+++ src/data/experienceData.js
@@ -0,0 +1,33 @@
+export const experienceData = [
+    // {
+    //     id: 1,
+    //     company: 'Globex Corporation',
+    //     jobtitle: 'Frontend Developer',
+    //     startYear: '2018',
+    //     endYear: '2019'
+    // },
+    // {
+    //     id: 2,
+    //     company: 'Vehement Capital Partners',
+    //     jobtitle: 'Backend Developer',
+    //     startYear: '2019',
+    //     endYear: '2020'
+    // },
+    {
+        id: 2,
+        company: 'YapBuzz LLC',
+        jobtitle: 'Jr. NodeJs Developer',
+        startYear: ' 06 JUN 2022 ',
+        endYear: ' Present ',
+        address: "Emory, Texas, United States"
+    },
+    {
+        id: 1,
+        company: 'Ricomputech Private Limited',
+        jobtitle: 'Intern NodeJs Developer',
+        startYear: ' 01 JUN 2022 ',
+        endYear: ' 06 JUN 2022 ',
+        address: "Kolkata, West Bengal, India"
+    },
+
+]
